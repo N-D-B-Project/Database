ead0c4d5c51b189d10a46e50756c8a9179abce8d
NedcloarBR <nedcloar1@hotmail.com>
2026-04-21 01:29:25 -0300

    feat(tickets): add ticket system schema and migrations

---
+++ prisma/migrations/20260421032746_add_support_id_in_tickets/migration.sql
@@ -0,0 +1,3 @@
+-- AlterTable
+ALTER TABLE "TicketType" ADD COLUMN     "categoryId" TEXT,
+ADD COLUMN     "supportRoleId" TEXT;
---
+++ prisma/migrations/20260421035140_tickets_system/migration.sql
@@ -0,0 +1,4 @@
+-- AlterTable
+ALTER TABLE "GuildSettings" ADD COLUMN     "ticketPanelColor" VARCHAR(7),
+ADD COLUMN     "ticketPanelDesc" TEXT,
+ADD COLUMN     "ticketPanelTitle" TEXT;
